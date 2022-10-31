cce92b3509928a0fd4f2e5a0326b1c80d202218f
Shahin <shahin@Shahins-MacBook-Pro.local>
2022-10-31 11:41:38 +0530

    initial-commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+      // When the user scrolls the page, execute myFunction
+      window.onscroll = function() {myFunction()};
+
+      // Get the header
+      var header = document.getElementById("Main-header");
+      
+      // Get the offset position of the navbar
+      var sticky = header.offsetTop;
+      
+      // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+      function myFunction() {
+        if (window.pageYOffset > sticky) {
+          header.classList.add("sticky");
+        } else {
+          header.classList.remove("sticky");
+        }
+      }
+      $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+      })
+      $('html').click(function() {
+        $('.nav-dropdown').hide();
+      });
+});
